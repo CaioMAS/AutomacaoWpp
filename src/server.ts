7696fa0c248bdba41d51f11bf201e1d340910cf1
Caio <caiosantos.eng@hotmail.com>
2025-10-14 11:55:57 -0300

    colocando lembrete 24h

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,31 @@
+// src/server.ts
 import dotenv from 'dotenv';
 dotenv.config();
+
 import app from './app';
 import { startWhatsApp } from './services/whatsappService';
 import { iniciarMonitoramentoDeLembretes } from './services/reminderScheduler';
 import { iniciarCheckMeetingsMissingDay8h } from './services/checkMeetingsMissingDay';
 
+import cron from 'node-cron';
+import { checkMeetingsMissing24Hours } from './services/checkMeetingsMissing24Hours';
 
-startWhatsApp(); // ⬅️ inicia o cliente assim que o servidor sobe
+startWhatsApp();
 iniciarMonitoramentoDeLembretes(5);
-iniciarCheckMeetingsMissingDay8h(); 
+iniciarCheckMeetingsMissingDay8h();
+
+// 24h antes, a cada 15 min
+cron.schedule('*/15 * * * *', async () => {
+  try {
+    console.log('⏰ Executando lembretes de 24h antes...');
+    await checkMeetingsMissing24Hours();
+    console.log('✅ Lembretes 24h processados.');
+  } catch (err) {
+    console.error('❌ Erro no 24h:', err);
+  }
+});
 
 const PORT = process.env.PORT || 5555;
-
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
-
