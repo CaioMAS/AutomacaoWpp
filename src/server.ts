1256209a812d6d6e8f908e46a9c9c8bb13bd09dc
Caio <caiosantos.eng@hotmail.com>
2025-10-23 10:37:58 -0300

    alteracao no soft delete sem comentarios

--- src/server.ts
+++ src/server.ts
@@ -19,42 +19,42 @@ iniciarCheckMeetingsMissingDay8h();
 //enviarMensagemMotivacionalAgora();
 
 // Agenda o envio diário às 08:00 da manhã (horário de Brasília)
-// cron.schedule("0 8 * * *", async () => {
-//   try {
-//     console.log("⏰ Enviando mensagem motivacional diária (08:00 BRT)...");
-//     await enviarMensagemMotivacionalAgora();
-//     console.log("✅ Mensagem motivacional enviada com sucesso!");
-//   } catch (e: any) {
-//     console.error("❌ Erro ao enviar mensagem motivacional:", e?.message || e);
-//   }
-// }, {
-//   timezone: "America/Sao_Paulo"
-// });
+cron.schedule("0 8 * * *", async () => {
+  try {
+    console.log("⏰ Enviando mensagem motivacional diária (08:00 BRT)...");
+    await enviarMensagemMotivacionalAgora();
+    console.log("✅ Mensagem motivacional enviada com sucesso!");
+  } catch (e: any) {
+    console.error("❌ Erro ao enviar mensagem motivacional:", e?.message || e);
+  }
+}, {
+  timezone: "America/Sao_Paulo"
+});
 
 // // roda a cada 5 minutos pra capturar a janela ~30m
-// cron.schedule(
-//   '*/5 * * * *',
-//   async () => {
-//     await enviarReminders30mAgora({
-//       instancia: 'AgenteIA',            // opcional, senão usa FIXED_INSTANCIA
-//       numeroDestino: '553399501851',    // opcional, senão usa FIXED_NUMERO
-//       tz: 'America/Sao_Paulo',
-//       janelaMinutos: { min: 29, max: 31 },
-//     });
-//   },
-//   { timezone: 'America/Sao_Paulo' }
-// );
+cron.schedule(
+  '*/5 * * * *',
+  async () => {
+    await enviarReminders30mAgora({
+      instancia: 'AgenteIA',            // opcional, senão usa FIXED_INSTANCIA
+      numeroDestino: '553399501851',    // opcional, senão usa FIXED_NUMERO
+      tz: 'America/Sao_Paulo',
+      janelaMinutos: { min: 29, max: 31 },
+    });
+  },
+  { timezone: 'America/Sao_Paulo' }
+);
 
 // //24h antes, a cada 15 min
-// cron.schedule('*/15 * * * *', async () => {
-//   try {
-//     console.log('⏰ Executando lembretes de 24h antes...');
-//     await checkMeetingsMissing24Hours();
-//     console.log('✅ Lembretes 24h processados.');
-//   } catch (err) {
-//     console.error('❌ Erro no 24h:', err);
-//   }
-// });
+cron.schedule('*/15 * * * *', async () => {
+  try {
+    console.log('⏰ Executando lembretes de 24h antes...');
+    await checkMeetingsMissing24Hours();
+    console.log('✅ Lembretes 24h processados.');
+  } catch (err) {
+    console.error('❌ Erro no 24h:', err);
+  }
+});
 
 const PORT = process.env.PORT || 5555;
 app.listen(PORT, () => {
